bef928dd43ad13a613558872bb78fd76d8b8f327
Mark Kuznetsov <mkuz.fl@gmail.com>
2023-09-12 14:00:15 -0600

    ✔️

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,12 +1,6 @@
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div class="home-card p-5 bg-white rounded elevation-3">
-      <img src="https://bcw.blob.core.windows.net/public/img/8600856373152463" alt="CodeWorks Logo"
-        class="rounded-circle">
-      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
-        <button class="btn btn-success" @click="getGifts()">Get GIFTS</button>
-      </h1>
-    </div>
+  <div class="col-4 gift-cont p-0 m-0">
+
   </div>
 </template>
 
